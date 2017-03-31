e090f735985438de499f7bb7414489aba5b6c796
Srisarguru Sridhar <srisargurusridhar@u.boisestate.edu>
2017-03-31 15:23:06 -0400

    added slide show and folders

--- scripts/app.js
+++ scripts/app.js
@@ -23,10 +23,33 @@ function Content(){
             this.aboutvar=true;
         }
        
-    }
+    };
 this.isActive = function (viewLocation) {
      var active = (viewLocation === this.location.path());
      return active;
 };
+
+var slideIndex = 0;
+showSlides();
+this.showclick=function(n) {
+    slideIndex=n-1;
+}
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex> slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
+
 
 }
